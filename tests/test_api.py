f35bee357672051c4f35d3bffd88d23d2ef6b2a6
Варвара Куракова <kuravar@gmail.com>
2025-08-05 20:57:09 +0300

    new project

--- tests/test_api.py
+++ tests/test_api.py
@@ -8,7 +8,7 @@ from httpx import AsyncClient
 
 @pytest.mark.asyncio
 async def test_task_lifecycle():
-    async with AsyncClient(base_url="http://localhost:8000") as ac:
+    async with AsyncClient(app=app, base_url="http://test") as ac:
         payload = {
             "timeoutInSeconds": 5,
             "parameters": {
